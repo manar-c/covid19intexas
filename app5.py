0eeb5db355e2911f5fd941387171955bf7e2a16f
Manar <mchammas@d-matrix.ai>
2020-04-11 15:08:55 -0500

    updated dallas

--- app5.py
+++ app5.py
@@ -284,7 +284,7 @@ app.layout = html.Div(style={'backgroundColor':colors['background'],'textAlign':
                                                        'backgroundColor':'lightgrey'},
                              className=spacing),
                     html.Div([html.P(html.B('Dallas County')),
-                              html.P('Updated: 4/10/2020'),
+                              html.P('Updated: 4/11/2020'),
                               html.P('Total: '+str(dallasToday)),
                                html.P(['New: ',html.Span(str(dallasNewCasesToday), style={'color':dallasincrease})])],
                     
