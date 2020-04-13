cce95fbee74fb26dcbe141b68ed506be99c385f8
Manar <mchammas@d-matrix.ai>
2020-04-13 10:43:59 -0500

    updated data

--- app5.py
+++ app5.py
@@ -294,7 +294,7 @@ app.layout = html.Div(style={'backgroundColor':colors['background'],'textAlign':
                                                        'backgroundColor':'lightgrey'},
                              className=spacing),
                     html.Div([html.P(html.B('Harris County')),
-                              html.P('Updated: 4/11/2020'),
+                              html.P('Updated: 4/12/2020'),
                               html.P('Total: '+str(harrisToday)),
                                html.P(['New: ',html.Span(str(harrisNewCasesToday), style={'color':harrisincrease})])],
                          
