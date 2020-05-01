0aa9aeb3639f55d60c78f9747f70e8f4cd44e21d
Manar <mchammas@d-matrix.ai>
2020-04-30 20:02:16 -0500

    some updates

--- app5.py
+++ app5.py
@@ -265,7 +265,7 @@ app.layout = html.Div(style={'backgroundColor':colors['background'],'textAlign':
                 html.Div([
                     html.Div(),
                     html.Div([html.P(html.B('Texas')),
-                              html.P('Updated: 4/29/2020'),
+                              html.P('Updated: 4/30/2020'),
                               html.P('Total: '+str(texasToday)),
                               html.P(['New: ',html.Span(str(texasNewCasesToday), style={'color':texasincrease})])], style={'textAlign':"center",
                                                        'borderRadius':'4px','border':'solid darkgrey',
@@ -274,7 +274,7 @@ app.layout = html.Div(style={'backgroundColor':colors['background'],'textAlign':
                                                        'backgroundColor':'lightgrey'},
                              className=spacing),
                     html.Div([html.P(html.B('Travis County')),
-                              html.P('Updated: 4/29/2020'),
+                              html.P('Updated: 4/30/2020'),
                               html.P('Total: '+str(austinToday)),
                               html.P(['New: ',html.Span(str(austinNewCasesToday), style={'color':austinincrease})])],
                              style={'textAlign':"center",
@@ -294,7 +294,7 @@ app.layout = html.Div(style={'backgroundColor':colors['background'],'textAlign':
                                                        'backgroundColor':'lightgrey'},
                              className=spacing),
                     html.Div([html.P(html.B('Harris County')),
-                              html.P('Updated: 4/29/2020'),
+                              html.P('Updated: 4/30/2020'),
                               html.P('Total: '+str(harrisToday)),
                                html.P(['New: ',html.Span(str(harrisNewCasesToday), style={'color':harrisincrease})])],
                          
