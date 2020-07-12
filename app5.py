d01815618f149eebb2451860653a4479bcdb5992
Manar <mchammas@d-matrix.ai>
2020-07-12 14:17:09 -0500

    fix

--- app5.py
+++ app5.py
@@ -358,8 +358,18 @@ var sc_invisible=1;
 var sc_security="5c457a33"; 
 </script>
 <script type="text/javascript"
-src="https://www.statcounter.com/counter/counter.js"
+src="https://secure.statcounter.com/counter/counter.js"
 async></script>
+
+<script type="text/javascript"
+src="https://secure.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics
+Made Easy - StatCounter" href="https://statcounter.com/"
+target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/12224865/0/5c457a33/1/"
+alt="Web Analytics Made Easy -
+StatCounter"></a></div></noscript>
 <!-- End of Statcounter Code -->
     </body>
 </html>
